e7f62cdab865064dfa91a509d19398cd4a81d793
Divakar <divakar39312@gmail.com>
2022-08-04 13:22:02 +0530

    [Divakar]:UC2 added

--- src/com/Bridgelabz/Tictactoe.java
+++ src/com/Bridgelabz/Tictactoe.java
@@ -17,11 +17,25 @@ private static char[] board = new char[10];
     		board[i] = ' ';
     	}		
 	}
+    /**
+     * UC2 Taking playing symbol from user as X or O
+     */
+    static void playerInput() {
+		System.out.println("player 1 please select the symbbol(x/o):");
+		player1=sc.next().charAt(0);
+		if(player1 =='X' || player1=='O') {
+			System.out.println("player 1 symbol :" + player1);
+		}else {
+			System.out.println("invalid symbol please enter x or o");
+			playerInput();
+		}
+	}
     
 	public static void main(String[] args) {
 		System.out.println("welcome to tictactoe simulation");
 		startGame();
 
+		playerInput();
 	}
 
 }
